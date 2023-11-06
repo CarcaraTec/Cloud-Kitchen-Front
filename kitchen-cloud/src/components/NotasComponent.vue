147e8a4b425f305ec2e2d46a47b2b61ba9371dec
Igor Suzuki Kira <89170220+igorsuzuki99@users.noreply.github.com>
2023-11-05 23:04:43 -0300

    Placeholder no datepicker

--- kitchen-cloud/src/components/NotasComponent.vue
+++ kitchen-cloud/src/components/NotasComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>
-    <VueDatePicker :model-value="selected" format="dd/MM/yyyy" locale="br" @update:model-value="dateSelected" range class="custom-datepicker"/>
+    <VueDatePicker :model-value="selected" format="dd/MM/yyyy" locale="br" @update:model-value="dateSelected" range :placeholder="'Escolha um período'" class="custom-datepicker"/>
     </div>
     <div class="card-container">
       <CardReceitaTotal :receitaTotal="cardData.receitaTotal" />
