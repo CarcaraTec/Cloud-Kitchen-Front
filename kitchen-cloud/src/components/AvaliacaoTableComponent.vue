c83bfe96081b4d73c65a4790d46d5e6e65afc422
Igor Suzuki Kira <89170220+igorsuzuki99@users.noreply.github.com>
2023-11-05 21:16:42 -0300

    CCC-93 Tabela de avaliações

--- kitchen-cloud/src/components/AvaliacaoTableComponent.vue
+++ kitchen-cloud/src/components/AvaliacaoTableComponent.vue
@@ -2,18 +2,18 @@
     <table class="table-data">
         <thead>
         <tr>
-            <th>ID</th>
+            <th>Cód Nota</th>
             <th>Nota atendimento</th>
             <th>Comentário</th>
             <th>Data da avaliação</th>
         </tr>
         </thead>
         <tbody>
-            <tr v-for="item in filteredFuncionario" :key="item.cod">
-                <td>{{ item.id }}</td>
-                <td>{{ item.nome }}</td>
-                <td>{{ item.funcao }}</td>
-                <td>{{ item.diaFolga }}</td>
+            <tr v-for="item in filteredFuncionario" :key="item.codNota">
+                <td>{{ item.codNota }}</td>
+                <td>{{ item.notaAtendimento/2 }}</td>
+                <td>{{ item.comentario }}</td>
+                <td>{{ formatDateTime(item.dataAvaliacao) }}</td>
             </tr>
         </tbody>
     </table>
@@ -29,10 +29,14 @@ data() {
     searchTerm: '',
   };
 },
+created() {
+  const funcionarioId = this.$route.params.id;
+  this.fetchData(funcionarioId);
+},
 computed: {
   filteredFuncionario() {
     const termo = this.searchTerm.toLowerCase();
-    return this.funcionarioData.filter(item => item.nome.toLowerCase().includes(termo));
+    return this.funcionarioData.filter(item => item.comentario.toLowerCase().includes(termo));
   }
 },
 
@@ -40,14 +44,41 @@ mounted() {
   this.fetchData();
 },
 methods: {
-  fetchData() {
-    axios.get('http://localhost:8081/funcionarios')
+  fetchData(id) {
+    axios.get(`http://localhost:8081/funcionarios/avaliacao/${id}`)
     .then(response => {
       this.funcionarioData = response.data;
     })
     .catch(error => {
       console.error('Erro ao realizar busca dos funcionários: ', error);
     });
+  },
+  filterDataByDate() {
+    if (!this.selectedDate) {
+      // Se a data não estiver selecionada, exiba todos os dados
+      return this.notasData;
+    } else {
+      // Se uma data estiver selecionada, filtre os dados com base nessa data
+      const selectedDate = new Date(this.selectedDate).toISOString().split('T')[0];
+      return this.notasData.filter((item) => {
+        const itemDate = item.abertura.split('T')[0]; // Supondo que a data está no formato ISO
+        return itemDate === selectedDate;
+      });
+    }
+  },
+  formatDateTime(dateTime) {
+    if (!dateTime) return '';
+
+    const date = new Date(dateTime);
+    const options = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    };
+
+    return date.toLocaleString('pt-BR', options);
   },
 }
 
